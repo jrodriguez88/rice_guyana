35d0da30289ec5ee97de96ed9049ac61a9d8597a
jrodriguez88 <jeferson_88@hotmail.com>
2019-05-31 15:09:26 -0500

    climate plots finish

--- final_plots.R
+++ final_plots.R
@@ -3,17 +3,6 @@
 library(lubridate)
 library(plotly)
 
-all_data %>% unnest(data) %>%
-    group_by(year(date), month(date), Loc, Region) %>%
-    summarise(sum(rain)) %>% ungroup() %>% setNames(c("year", "month", "loc", "region", "rain")) %>%
-    ggplot(aes(month, rain, color=factor(year))) + 
-    geom_line()+
-    geom_point(aes(color=factor(year))) +
-    stat_summary(aes(group=month), fun.data = mean_cl_boot) + 
-    scale_x_continuous(breaks = 1:12, labels=month.abb) +
-    theme_bw() +
-    facet_wrap(loc ~ .)
-
 
 summary_df <- all_data %>% unnest(data) %>%
     group_by(year(date), month(date), Loc, Region) %>%
@@ -81,5 +70,41 @@ all_data %>% filter(Loc=="Boerasire") %>% unnest(data) %>%
 #    scale_x_continuous(breaks = 0:12) +
     theme_bw()
 
+
+
+all_weather_plots <- summary_df %>%
+    gather("var", "value", -c(1:4)) %>% nest(-c(var)) %>%
+    mutate(plots = map(data, ~ggplot(data = .x, aes(month, value, color=factor(year))) + 
+    geom_line()+
+    geom_point(aes(color=factor(year))) +
+    stat_summary(aes(group=month), fun.data = mean_cl_boot) + 
+    scale_x_continuous(breaks = 1:12, labels=month.abb) +
+    theme_bw() +
+    facet_wrap(loc ~ .) +
+    theme(
+        legend.position = c(0.85, 0.22),
+        axis.text.x = element_text(angle = 90),
+        #        legend.position="bottom",
+        #        legend.title = element_blank(),
+        panel.grid.minor = element_blank(),
+        strip.background=element_rect(fill="white", size=1.5, linetype="solid"),
+        strip.text = element_text(face = "bold")) +  
+    labs(x = "Month", 
+#         y= "Value - rain(mm), srad(MJ/m²d), temp(oC)", 
+#         title = "Guyana Climate Data",
+         color = "Year")))
+
+
+# rain 
+all_weather_plots$plots[[1]] + labs(y="Rain (mm)", title = "Anything")
+
+# tmax 
+all_weather_plots$plots[[2]] + labs(y="Tmax (oC)", title = "Anything")
+
+# tmin 
+all_weather_plots$plots[[3]] + labs(y="Tmin (oC)", title = "Anything")
+
+# srad 
+all_weather_plots$plots[[4]] + labs(y="Srad (MJ/m²d)", title = "Anything")
 
 
